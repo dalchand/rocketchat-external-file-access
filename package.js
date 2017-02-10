f56b49a03c3b1b286702e206e79c264c897e3434
Dal Chand <dc4ual@gmail.com>
2017-02-10 21:11:45 +0530

    added git repo in package.js

--- package.js
+++ package.js
@@ -4,7 +4,7 @@ Package.describe({
   // Brief, one-line summary of the package.
   summary: 'A package for providing file access tokens for external services eg. facebook messenger',
   // URL to the Git repository containing the source code for this package.
-  git: '',
+  git: 'https://github.com/dalchand/rocketchat-external-file-access.git',
   // By default, Meteor will default to using README.md for documentation.
   // To avoid submitting documentation, set this field to null.
   documentation: 'README.md'
